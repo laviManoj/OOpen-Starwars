3751fe8e1279aeb176f23866a6fc4f9146d70596
lavimanoj <lavigeremanoj@gmail.com>
2024-02-05 23:43:50 +0530

    Added the Login Code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './component/LogInPage/LoginPage';
+import RegisterPage from './component/RegisterPage';
 
-function App() {
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+    <Router>
+      <Routes>
+     
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/User-Register/" element={<RegisterPage />} />
+        
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
